131540fae61a11fa5ffc74be93c22dae0eeb4224
MadhurNehete <132289905+MadhurNehete@users.noreply.github.com>
2026-03-26 12:02:48 +0530

    Linter-Fix2

--- crossIndex/reindexer/reindexer.go
+++ crossIndex/reindexer/reindexer.go
@@ -146,7 +146,7 @@ func indexEnergyBlockInfo(energyBlockInfo *data.BlockInfo, epoch uint32, esClien
 }
 
 func (r *reindexer) checkAndCreateValuesIndex() error {
-	template, err := readTemplateForIndex(r.pathToIndicesConfig, valuesIndex) //nolint:ineffassign
+	template, err := readTemplateForIndex(r.pathToIndicesConfig, valuesIndex) //nolint:ineffassign,SA4006
 	templateBytes := template.Bytes()
 
 	for _, dstClient := range r.destinationClients {
